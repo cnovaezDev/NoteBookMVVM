03a75d73751a8002fe6b11c3138fa795c3785296
cnovaezDev <cnovaez.dev@gmail.com>
2022-07-25 11:15:20 -0400

    Fixed bottom logs functionality

--- app/src/main/java/cnovaez/dev/notebookmvvm/ui/views/components/DialogBottomSheetLogs.kt
+++ app/src/main/java/cnovaez/dev/notebookmvvm/ui/views/components/DialogBottomSheetLogs.kt
@@ -5,7 +5,9 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,36 +20,54 @@ import cnovaez.dev.notebookmvvm.ui.adapter.logs.LogsAdapter
 import cnovaez.dev.notebookmvvm.ui.viewmodels.logs.LogsViewModel
 import cnovaez.dev.notebookmvvm.utils.types.LogActions
 import cnovaez.dev.notebookmvvm.utils.types.NoteActionType
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DialogBottomSheetLogs() : DialogFragment() {
+class DialogBottomSheetLogs() : BottomSheetDialogFragment() {
 
 
-    private lateinit var binding: LogsDialogBinding
+    private var _binding: LogsDialogBinding? = null
+    private val binding get() = _binding!!
+
     private val logsViewModel: LogsViewModel by viewModels()
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        super.onCreateDialog(savedInstanceState)
-        binding = LogsDialogBinding.inflate(layoutInflater)
-        val builder = AlertDialog.Builder(requireActivity())
-        builder.setView(binding.root)
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = LogsDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
         logsViewModel.logsModel.observe(this) { logs ->
             initRecyclerView(logs)
             binding.noLogstv.visibility = if (logs.size > 0) View.GONE else View.VISIBLE
-           // binding.cardRecyclerLogsCv.visibility = if (logs.size == 0) View.GONE else View.VISIBLE
+            // binding.cardRecyclerLogsCv.visibility = if (logs.size == 0) View.GONE else View.VISIBLE
         }
 
         logsViewModel.getAllLogs()
-
-        //for transparent background
-        val dialog = builder.create()
-        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        return dialog
     }
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        super.onCreateDialog(savedInstanceState)
+//        binding = LogsDialogBinding.inflate(layoutInflater)
+//        val builder = AlertDialog.Builder(requireActivity())
+//        builder.setView(binding.root)
+//
+//
+//
+//        //for transparent background
+//        val dialog = builder.create()
+//        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//
+//        return dialog
+//    }
 
     fun initRecyclerView(logs: List<Log>) {
         val recyclerView = binding.logListRv
